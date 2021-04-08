8544a9f11e45dd77102962e40068c3b9d6d7c195
tversic <tomeversic2001@gmail.com>
2021-04-08 16:40:37 +0200

    test1

--- src/main/java/com/m2/cfg/MyRunner.java
+++ src/main/java/com/m2/cfg/MyRunner.java
@@ -1,6 +1,5 @@
 package com.m2.cfg;
 
-import com.m2.cfg.domain.Users;
 import com.m2.cfg.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
