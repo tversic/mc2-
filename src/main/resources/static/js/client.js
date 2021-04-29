78dc10b961627112e740c69487e0cc4275b20e6e
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 16:07:49 +0200

    Napravljen videocall za 1 on 1 NOTE: u client.js treba mjenjati var conn

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,5 +1,11 @@
-//connecting to our signaling server
-var conn = new WebSocket('wss://localhost:8443/socket');
+//ovaj connection koristiti kada se koristi u LAN-u
+var conn = new WebSocket('wss://192.168.5.11:8443/socket');
+//var conn = new WebSocket('wss://localhost:8443/socket');
+
+const messageBox = document.getElementById('messageBox')
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
 
 
 conn.onopen = function() {
@@ -45,6 +51,17 @@ function initialize() {
 
     peerConnection = new RTCPeerConnection(configuration);
 
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).
+    then(stream => {
+        addVideoStream(myVideo, stream)
+        stream.getTracks().forEach(track => peerConnection.addTrack(track,stream))
+    }).catch(function(err) {
+        /* handle the error */
+    });
+
     // Setup ice handling
     peerConnection.onicecandidate = function(event) {
         if (event.candidate) {
@@ -60,6 +77,7 @@ function initialize() {
         reliable : true
     });
 
+
     dataChannel.onerror = function(error) {
         console.log("Error occured on datachannel:", error);
     };
@@ -67,6 +85,7 @@ function initialize() {
     // when we receive a message from the other peer, printing it on the console
     dataChannel.onmessage = function(event) {
         console.log("message:", event.data);
+        messageBox.append("them:"+event.data);
     };
 
     dataChannel.onclose = function() {
@@ -77,7 +96,23 @@ function initialize() {
         dataChannel = event.channel;
     };
 
+    /*peerConnection.ontrack = (event) => {
+        const video = document.createElement('video');
+         if (event.streams && event.streams[0]) {
+             addVideoStream(video,event.streams[0]);
+         }
+    };*/
+
+    const remoteStream = new MediaStream();
+    const remoteVideo = document.querySelector('#remoteVideo');
+    remoteVideo.srcObject = remoteStream;
+
+    peerConnection.addEventListener('track', async (event) => {
+        remoteStream.addTrack(event.track);
+    });
+
 }
+
 
 function createOffer() {
     peerConnection.createOffer(function(offer) {
@@ -122,5 +157,15 @@ function handleAnswer(answer) {
 
 function sendMessage() {
     dataChannel.send(input.value);
+    messageBox.append("me:"+input.value);
     input.value = "";
+}
+
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
 }
